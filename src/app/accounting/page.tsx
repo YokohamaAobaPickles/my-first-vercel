751def70fee934bc512c7666e3428334aaae68f5
YokohamaAobaPickles <yokohama.aoba.pickles@gmail.com>
2026-02-19 11:02:28 +0900

    link to mockup page

--- src/app/accounting/page.tsx
+++ src/app/accounting/page.tsx
@@ -4,6 +4,14 @@
  * Update  : 2026-02-11
  * Remarks : V0.1.0 - D群開発用ダミーファイル
  */
+
+import React from 'react';
+import { baseStyles } from '@/types/styles/style_common';
+
 export default function AccountingPage() {
   return <div>会計管理（準備中）</div>;
+  <a href="/test/image_sample" style={baseStyles.Link}>
+    イメージサンプル
+  </a>  
+
 }
--- src/app/assets/page.tsx
+++ src/app/assets/page.tsx
@@ -4,6 +4,15 @@
  * Update  : 2026-02-11
  * Remarks : V0.1.0 - D群開発用ダミーファイル
  */
+import React from 'react';
+import { baseStyles } from '@/types/styles/style_common'
+
+
 export default function AssetsPage() {
-  return <div>資源・備品管理（準備中）</div>;
+  return <div>資源・備品管理（準備中）
+    <p>このページは、資源・備品管理機能の開発に向けたダミーファイルです。</p>
+  </div>;
+  <a href="/test/mockup" style={baseStyles.Link}>
+    モックアップ
+  </a>  
 }
